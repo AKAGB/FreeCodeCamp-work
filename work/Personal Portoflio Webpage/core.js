0be9f322daed731e8edb2beab5a17011fb091100
AKAGB <374118168@qq.com>
2018-04-29 03:15:57 +0800

    Per Completed

--- work/Personal Portoflio Webpage/core.js
+++ work/Personal Portoflio Webpage/core.js
@@ -1,9 +1,19 @@
 $(function () {
     $('.navbar-nav li').click(function () {
+        var a = $(this).find('a'),
+            h = parseFloat($('.navbar-header').css('height'));
         $(this).siblings().removeClass('active');
         $(this).addClass('active');
+        console.log(parseInt(window.outerWidth));
         if (parseInt(window.outerWidth) < 800) {
             $('.navbar-toggle').click();
         }
+        $('html, body').stop().animate({
+            scrollTop: '' + parseFloat($(a.attr('href')).offset().top) - h
+        }, 500);
+    });
+
+    $('#icon').click(function () {
+        $('#rhome').click();
     });
 });
