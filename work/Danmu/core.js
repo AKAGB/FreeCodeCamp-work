b80d5d1782489fc8b590c01c8e91310da4e7dc95
AKAGB <374118168@qq.com>
2018-05-03 17:43:45 +0800

    change color

--- work/Danmu/core.js
+++ work/Danmu/core.js
@@ -1,6 +1,7 @@
 $(function () {
     var input = $('.dm_string');
     var fontColor = ['skyblue', '#aaa', '#faa'];
+    var cnt = 0;
     $('.dm_send').click(function () {
         if (input.val() != '') {
             var newEle = $('<div class="dm-text"></div>');
@@ -8,11 +9,12 @@ $(function () {
             input.val('');
             $('.show').append(newEle);
             newEle.css('top', Math.random()*90 + '%')
-                    .css('color', fontColor[Math.floor(Math.random()*3)])
+                    .css('color', fontColor[cnt%3])
                     .animate({}, function () {
                         newEle.css('transform', 'translate(-'+
                                         2 * window.outerWidth +'px)');
                     });
+            cnt++;
         }
     })
 });
