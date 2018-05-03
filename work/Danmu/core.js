a7a5616ef5bfbaec30f1dfb13a9885375ebd6016
AKAGB <374118168@qq.com>
2018-05-03 16:18:12 +0800

    Danmu complete

--- work/Danmu/core.js
+++ work/Danmu/core.js
@@ -1,12 +1,18 @@
 $(function () {
     var input = $('.dm_string');
-    $('.dm-text').ready(function () {
-        console.log('Hello');
-    });
+    var fontColor = ['skyblue', '#aaa', '#faa'];
     $('.dm_send').click(function () {
-        var newEle = $('<div class="dm-text"></div>');
-        newEle.html(input.val());
-        input.val('');
-        $('.show').append(newEle);
+        if (input.val() != '') {
+            var newEle = $('<div class="dm-text"></div>');
+            newEle.html(input.val());
+            input.val('');
+            $('.show').append(newEle);
+            newEle.css('top', Math.random()*90 + '%')
+                    .css('color', fontColor[Math.floor(Math.random()*3)])
+                    .animate({}, function () {
+                        newEle.css('transform', 'translate(-'+
+                                        2 * window.outerWidth +'px)');
+                    });
+        }
     })
 });
