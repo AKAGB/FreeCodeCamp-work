55187715874ae548a875f21531ab3834990587a1
AKAGB <374118168@qq.com>
2019-03-12 11:01:40 +0800

    Wiki Start

---
+++ work/Wikipedia Viewer/core.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('.icon-search').click(function () {
+        $('.icon-search').animate({width: '250px'});
+        $('.bar').hide();
+        $('.form-search').removeAttr('hidden').focus();
+        $('.rs-btn').removeAttr('hidden');
+    });
+
+    $('.rs-btn').click(function (e) {
+        e.stopPropagation();
+        $('.form-search').val('').attr({'hidden': 'hidden'});
+        $('.rs-btn').attr({'hidden': 'hidden'});
+        $('.icon-search').animate({width: '36px'}, function() {
+            $('.bar').fadeIn();
+        });
+        
+    });
+});
