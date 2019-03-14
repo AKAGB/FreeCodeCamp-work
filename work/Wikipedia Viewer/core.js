c57734b3b82b9fa77407185e1b7b66c5a7002a1d
AKAGB <374118168@qq.com>
2019-03-15 00:43:42 +0800

    Wiki Version2

--- work/Wikipedia Viewer/core.js
+++ work/Wikipedia Viewer/core.js
@@ -14,4 +14,7 @@ $(function () {
             $('.bar').fadeIn();
         });
     });
+
 });
+
+// https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=buffer&callback=JSON_CALLBACK
